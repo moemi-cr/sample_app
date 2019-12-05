36769af46bf526f1938b2fc153d39d2c80d3e0b4
masaya.ishioka <masaya.ishioka@pa-rk.co.jp>
2019-12-05 01:07:27 +0000

    contactPage

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @baseTitle ="Ruby on Rails Tutorial Sample App"
+  end 
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -20,6 +22,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@baseTitle}"
+  end
+
+  test "should get contact" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "Contact | #{@baseTitle}"
   end
 
 end
