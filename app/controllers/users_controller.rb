4733fbb7ff638039118edd1f5ebc50f72e420a41
masaya.ishioka <masaya.ishioka@pa-rk.co.jp>
2019-12-11 02:15:41 +0000

    10.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  def index
+    @users = User.all
+  end  
   def show
     @user = User.find(params[:id])
   end
@@ -43,6 +46,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] =  "Please log in."
         redirect_to login_url
       end
