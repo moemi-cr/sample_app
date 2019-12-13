f291cb04ca7068782fa3690838633c497f127b8f
masaya.ishioka <masaya.ishioka@pa-rk.co.jp>
2019-12-13 09:06:10 +0000

    active digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token #インスタンス変数を直接変更して操作ができるようにする
-
-  before_save { self.email = email.downcase } #メールアドレスを保存する前に小文字にする selfは現在のユーザーを指す
+  attr_accessor :remember_token, :activation_token #インスタンス変数を直接変更して操作ができるようにする
+  before_save :downcase_email
+  before_create :create_activation_digest
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #メールアドレスの正規表現
   validates :email, presence: true, length: { maximum: 255 },
@@ -49,4 +50,16 @@ class User < ApplicationRecord
     ##モデル内のremember_digest属性(カラム)の値をnilにする
     update_attribute(:remember_digest, nil)
   end
+
+  private
+    #メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+    
+    #有効化トークンとダイジェストを作成及び代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-<<<<<<< HEAD
-User.create!(name: "Example User", email: "example@railstutorial.org",
-    password: "foobar", password_confirmation: "foobar")
-
-99.times do |n|
-    name = Faker::Name.name
-    email = "example-#{n+1}@rails tutorial.org"
-    password = "password"
-    User.create!(name: name, email: email, password: password, password_confirmation: password)
-end        
-=======
 User.create!(name:  "Example User",
     email: "example@railstutorial.org",
     password:              "foobar",
     password_confirmation: "foobar",
-    admin:true )
+    admin: true,
+    activated: true,
+    activated_at: Time.zone.now )
 
-20.times do |n|
+99.times do |n|
 name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
 User.create!(name:  name,
       email: email,
       password:              password,
-      password_confirmation: password)
+      password_confirmation: password,
+      activated: true,
+      activated_at: Time.zone.now)
 end
->>>>>>> updating-users
