85c3f52878a85b30cc9a16bc904e4e290b650d9f
masaya.ishioka <masaya.ishioka@pa-rk.co.jp>
2019-12-20 00:18:25 +0000

    修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+              # ↑ラムダ式         ↑降順に並び替え
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
